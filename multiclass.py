d8bb5e8fd23f7abeed5758a8561b26b9b2db28e1
Manu mitz <manuelaschmitz@gmail.com>
2024-12-03 21:14:16 -0300

    Corrigido hiperparametros e adicionado avaliacao dos dados de teste

--- multiclass.py
+++ multiclass.py
@@ -1,37 +1,34 @@
 from ucimlrepo import fetch_ucirepo
-import pandas as pd
 from sklearn.model_selection import train_test_split
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 from rnn import categorical_crossentropy_loss, categorical_crossentropy_loss_derivative, NeuralNetwork
 
-# Fetch dataset
+# Carregar o dataset do zoologico
 zoo = fetch_ucirepo(id=111)
 
-# Data (as pandas dataframes)
-X = zoo.data.features  # Features
-y = zoo.data.targets    # Target
+# Selecionar variáveis independentes (X) e dependentes (y)
+X = zoo.data.features 
+y = zoo.data.targets   
 
-# Aplicando One-Hot Encoding à coluna 'legs'
+# Aplicando One-Hot Encoding
 X = pd.get_dummies(X, columns=['legs'], prefix='legs', dtype=int)
-
-# Convertendo y para One-Hot Encoding
 y_onehot = pd.get_dummies(y, columns=['type'], prefix='type', dtype=int)
 
 X = X.to_numpy()
 y_onehot = y_onehot.to_numpy()
 
 # Dividindo o dataset
-X_train_1, X_test, y_train_1, y_test = train_test_split(X, y_onehot, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y_onehot, test_size=0.2, random_state=42)
 
-X_train, X_validation, y_train, y_validation = train_test_split(X_train_1, y_train_1, test_size=0.2, random_state=42)
-
+# Definindo hiperparâmetros
 input_size = X_train.shape[1]
-hidden_size = 10
+hidden_size = 250 # Camada oculta
 output_size = y_train.shape[1]  # Número de classes
-learning_rate = 0.01
-epochs = 1000
+learning_rate = 0.01 # Taxa de aprendizado
+epochs = 70 # Épocas
 
 # Inicializando a rede neural
 nn = NeuralNetwork(input_size, hidden_size, output_size, learning_rate, output_activation='softmax')
@@ -40,12 +37,17 @@ nn = NeuralNetwork(input_size, hidden_size, output_size, learning_rate, output_a
 train_losses, test_losses = nn.train(X_train, y_train, epochs, 
                                      categorical_crossentropy_loss, 
                                      categorical_crossentropy_loss_derivative, 
-                                     X_test=X_validation, y_test=y_validation)
+                                     X_test=X_test, y_test=y_test)
 
-# Avaliação
-y_pred = nn.predict(X_validation, 'multiclass')
-accuracy = np.mean(np.argmax(y_validation, axis=1) == y_pred) * 100
-print(f"Acurácia no conjunto de teste: {accuracy:.2f}%")
+# Avaliação dos dados de treinamento
+y_pred_train = nn.predict(X_train, 'multiclass')
+accuracy_train = np.mean(np.argmax(y_train, axis=1) == y_pred_train) * 100
+print(f"Acurácia no conjunto de dados de treinamento: {accuracy_train:.2f}%")
+
+# Avaliação dos dados de teste
+y_pred_test = nn.predict(X_test, 'multiclass')
+accuracy_test = np.mean(np.argmax(y_test, axis=1) == y_pred_test) * 100
+print(f"Acurácia no conjunto de dados de teste: {accuracy_test:.2f}%")
 
 # Gráfico das perdas
 plt.plot(train_losses, label="Treinamento")
